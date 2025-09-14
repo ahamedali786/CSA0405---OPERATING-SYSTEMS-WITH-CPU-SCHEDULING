dd09ffc2607d4e42361056e8f680daf73278c094
SHAIK AHAMED ALI 192425106 <sahamedali0@gmail.com>
2025-09-14 23:37:14 +0530

    Add files via upload

---
+++ WORST_FIT_P21.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int b[20], p[20], nb, np;
+    int alloc[20];
+    int i, j;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &nb);
+    printf("Enter sizes of blocks:\n");
+    for (i = 0; i < nb; i++) scanf("%d", &b[i]);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &np);
+    printf("Enter sizes of processes:\n");
+    for (i = 0; i < np; i++) scanf("%d", &p[i]);
+
+    for (i = 0; i < np; i++) alloc[i] = -1;
+
+    for (i = 0; i < np; i++) {
+        int worst = -1;
+        for (j = 0; j < nb; j++) {
+            if (b[j] >= p[i]) {
+                if (worst == -1 || b[j] > b[worst]) worst = j;
+            }
+        }
+        if (worst != -1) {
+            alloc[i] = worst;
+            b[worst] -= p[i];
+        }
+    }
+
+    printf("\nProcess\tSize\tBlock\n");
+    for (i = 0; i < np; i++) {
+        if (alloc[i] != -1)
+            printf("%d\t%d\t%d\n", i + 1, p[i], alloc[i] + 1);
+        else
+            printf("%d\t%d\tNot Allocated\n", i + 1, p[i]);
+    }
+
+    return 0;
+}
