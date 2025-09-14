c0a71641694d9733065e5ec031a64347b141509b
SHAIK AHAMED ALI 192425106 <sahamedali0@gmail.com>
2025-09-14 21:28:03 +0530

    Update MULTITHREADING_P11.c

--- MULTITHREADING_P11.c
+++ MULTITHREADING_P11.c
@@ -21,3 +21,24 @@ int main() {
 
     return 0;
 }
+
+SAMPLE OUTPUT:
+Illustrating the concept of multithreading:
+
+Thread 1 starts:
+Thread 1: Number 1
+Thread 1: Number 2
+Thread 1: Number 3
+Thread 1: Number 4
+Thread 1: Number 5
+Thread 1 finished
+
+Thread 2 starts:
+Thread 2: Letter A
+Thread 2: Letter B
+Thread 2: Letter C
+Thread 2: Letter D
+Thread 2: Letter E
+Thread 2 finished
+
+Main program: all threads completed
