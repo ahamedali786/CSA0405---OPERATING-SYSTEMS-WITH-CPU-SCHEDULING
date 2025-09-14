26994d42c1164f0aa9b63c24b8d084e3751a3c1c
SHAIK AHAMED ALI 192425106 <sahamedali0@gmail.com>
2025-09-14 22:25:45 +0530

    Add files via upload

---
+++ PROCESS_SYNCHRONIZATION_MUTEX_P19.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int resource = 0;  
+    int mutex = 1;      
+    int choice;
+
+    printf("Process Synchronization Simulation (Mutex)\n");
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Process 1 access resource\n");
+        printf("2. Process 2 access resource\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                if (mutex == 1) {
+                    printf("Process 1 entering critical section.\n");
+                    mutex = 0;   
+                    resource++;
+                    printf("Process 1 updated resource: %d\n", resource);
+                    mutex = 1;   
+                    printf("Process 1 leaving critical section.\n");
+                } else {
+                    printf("Resource busy. Process 1 waiting...\n");
+                }
+                break;
+
+            case 2:
+                if (mutex == 1) {
+                    printf("Process 2 entering critical section.\n");
+                    mutex = 0;  
+                    resource++;
+                    printf("Process 2 updated resource: %d\n", resource);
+                    mutex = 1;   
+                    printf("Process 2 leaving critical section.\n");
+                } else {
+                    printf("Resource busy. Process 2 waiting...\n");
+                }
+                break;
+
+            case 3:
+                printf("Exiting simulation.\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while (choice != 3);
+
+    return 0;
+}
