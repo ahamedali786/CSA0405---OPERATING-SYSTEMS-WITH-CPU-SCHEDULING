82cedafcb986763aeffb781392c4912102739c20
SHAIK AHAMED ALI 192425106 <sahamedali0@gmail.com>
2025-09-14 22:27:00 +0530

    Update PROCESS_SYNCHRONIZATION_MUTEX_P19.c

--- PROCESS_SYNCHRONIZATION_MUTEX_P19.c
+++ PROCESS_SYNCHRONIZATION_MUTEX_P19.c
@@ -54,3 +54,34 @@ int main() {
 
     return 0;
 }
+
+SAMPLE INPUT:
+Process Synchronization Simulation (Mutex)
+
+Menu:
+1. Process 1 access resource
+2. Process 2 access resource
+3. Exit
+Enter choice: 1
+
+SAMPLE OUTPUT:
+Enter choice: 1
+Process 1 entering critical section.
+Process 1 updated resource: 1
+Process 1 leaving critical section.
+
+Menu:
+1. Process 1 access resource
+2. Process 2 access resource
+3. Exit
+Enter choice: 2
+Process 2 entering critical section.
+Process 2 updated resource: 2
+Process 2 leaving critical section.
+
+Menu:
+1. Process 1 access resource
+2. Process 2 access resource
+3. Exit
+Enter choice: 3
+Exiting simulation.
